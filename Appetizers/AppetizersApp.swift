5a23b3fdd2ab6d678508e39536cbb8e1693ad435
MesumAli2 <smesumali20@gmail.com>
2025-05-26 13:36:48 +0400

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Mesum Syed on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
